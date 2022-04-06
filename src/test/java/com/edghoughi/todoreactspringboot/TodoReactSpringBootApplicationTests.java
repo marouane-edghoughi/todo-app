1ea5f1a4d0cb4e583d028a5d0aedcdd6aaf5af4c
Marouane Edghoughi <marouane@edghoughi.com>
2022-04-06 04:06:16 +0000

    Bootstrap the app with Spring Initializr and CRA TypeScript

---
+++ src/test/java/com/edghoughi/todoreactspringboot/TodoReactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edghoughi.todoreactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoReactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
